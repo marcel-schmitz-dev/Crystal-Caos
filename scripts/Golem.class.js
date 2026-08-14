b05b4db819441295e78618223ebebb4acabb1b90
Marcel Schmitz <mschmitz287@gmail.com>
2026-08-14 15:27:49 +0200

    Bilder hinzugefügt

--- scripts/Golem.class.js
+++ scripts/Golem.class.js
@@ -2,7 +2,7 @@ class Golem extends MovableObject {
     constructor() {
         super().loadImage("assets/img/golem-left.png");
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 300 + Math.random() * 500;
     }
 
     werfen() {}
